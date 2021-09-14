dedae028e0ef82b2899cda6321cb12c12b39c1be
Jonathan <contact@grafikart.fr>
2021-09-14 18:08:13 +0200

    fix: copy paste on firefox

--- src/functions/browser.ts
+++ src/functions/browser.ts
@@ -1,8 +1,13 @@
 export async function copyToClipboard(text: string): Promise<true> {
-  const result = await navigator.permissions.query({ name: 'clipboard-write' })
-  if (result.state == 'granted' || result.state == 'prompt') {
+  try {
+    const result = await navigator.permissions.query({ name: 'clipboard-write' })
+    if (result.state == 'granted' || result.state == 'prompt') {
+      await navigator.clipboard.writeText(text)
+      return true
+    }
+    throw result
+  } catch (e) {
     await navigator.clipboard.writeText(text)
     return true
   }
-  throw result
 }
