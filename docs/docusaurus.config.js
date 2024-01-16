21b678cd3455ab57c84486ab41d68fee6cb2647e
Jonathan <contact@grafikart.fr>
2024-01-16 10:19:50 +0100

    chore: update docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,14 +1,14 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const {themes} = require('prism-react-renderer');
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Visual Editor",
   tagline: "Edit your page block by block",
-  url: "https://boxraiser.github.io/visual-editor",
+  url: "https://boxraiser.github.io",
   baseUrl: "/visual-editor/",
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
